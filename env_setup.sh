jilalitajdine/Exynos-7420-6.0.1

BUILD: initial setup
- add scripts & tools to compiles Images
- create output directory
- enable CCACHE for faster compile
- toolchain: gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu
https://releases.linaro.org/15.02/components/toolchain/binaries/aarch64-linux-gnu/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu.tar.xz

Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

env_setup.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+
+# Colorize and add text parameters
+export red=$(tput setaf 1)             #  red
+export grn=$(tput setaf 2)             #  green
+export blu=$(tput setaf 4)             #  blue
+export cya=$(tput setaf 6)             #  cyan
+export txtbld=$(tput bold)             #  Bold
+export bldred=${txtbld}$(tput setaf 1) #  red
+export bldgrn=${txtbld}$(tput setaf 2) #  green
+export bldblu=${txtbld}$(tput setaf 4) #  blue
+export bldcya=${txtbld}$(tput setaf 6) #  cyan
+export txtrst=$(tput sgr0)             #  Reset
+
+
+# check if ccache installed, if not install
+if [ ! -e /usr/bin/ccache ]; then
+	echo "You must install 'ccache' to continue.";
+	sudo apt-get install ccache
+fi
+
+# check if xmllint installed, if not install
+if [ ! -e /usr/bin/xmllint ]; then
+	echo "You must install 'xmllint' to continue.";
+	sudo apt-get install libxml2-utils
+fi
+
+echo "${bldcya}***** Clean up Environment before compile *****${txtrst}";
+
+
+# Make clean source
+read -t 5 -p "Make clean source, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+make clean;
+make distclean;
+make mrproper;
+fi;
+
+# clear ccache
+read -t 5 -p "Clear ccache but keeping the config file, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+ccache -C;
+fi;
+
+
+TARGET=$1
+if [ "$TARGET" != "" ]; then
+	echo
+        echo "Starting your build for $TARGET"
+else
+        echo ""
+        echo "You need to define your device target!"
+        echo "example: build_kernel.sh N920C"
+        exit 1
+fi
+
+
+# location
+	export KERNELDIR=`readlink -f .`;
+
+
+# set build variables
+BK=build_kernel
+export KCONFIG_NOTIMESTAMP=true
+export ARCH=arm64;
+export SUB_ARCH=arm64;
+
+# SM-N920 C/CD/G/I
+if [ "$TARGET" = "N920C" ] ; then
+	export KERNEL_CONFIG="SkyHigh_N920C_defconfig";
+fi;
+
+# build script
+export USER=`whoami`;
+export TMPFILE=`mktemp -t`;
+
+
+# system compiler
+export CROSS_COMPILE=/home/upintheair/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+
+# CPU Core
+export NUMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`;

SM-G920P/TeamSPR-old

Conflicts:
	build_kernel.sh

env_setup.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+
+# Colorize and add text parameters
+export red=$(tput setaf 1)             #  red
+export grn=$(tput setaf 2)             #  green
+export blu=$(tput setaf 4)             #  blue
+export cya=$(tput setaf 6)             #  cyan
+export txtbld=$(tput bold)             #  Bold
+export bldred=${txtbld}$(tput setaf 1) #  red
+export bldgrn=${txtbld}$(tput setaf 2) #  green
+export bldblu=${txtbld}$(tput setaf 4) #  blue
+export bldcya=${txtbld}$(tput setaf 6) #  cyan
+export txtrst=$(tput sgr0)             #  Reset
+
+
+# check if ccache installed, if not install
+if [ ! -e /usr/bin/ccache ]; then
+	echo "You must install 'ccache' to continue.";
+	sudo apt-get install ccache
+fi
+
+# check if xmllint installed, if not install
+if [ ! -e /usr/bin/xmllint ]; then
+	echo "You must install 'xmllint' to continue.";
+	sudo apt-get install libxml2-utils
+fi
+
+echo "${bldcya}***** Clean up Environment before compile *****${txtrst}";
+
+
+# Make clean source
+read -t 5 -p "Make clean source, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+make clean;
+make distclean;
+make mrproper;
+fi;
+
+# clear ccache
+read -t 5 -p "Clear ccache but keeping the config file, 5sec timeout (y/n)?";
+if [ "$REPLY" == "y" ]; then
+ccache -C;
+fi;
+
+
+TARGET=$1
+if [ "$TARGET" != "" ]; then
+	echo
+        echo "Starting your build for $TARGET"
+else
+        echo ""
+        echo "You need to define your device target!"
+        echo "example: build_kernel.sh G920P"
+        exit 1
+fi
+
+
+# location
+	export KERNELDIR=`readlink -f .`;
+
+
+# set build variables
+BK=build_kernel
+export KCONFIG_NOTIMESTAMP=true
+export ARCH=arm64;
+export SUB_ARCH=arm64;
+
+# SM-G920P
+if [ "$TARGET" = "G920P" ] ; then
+	export KERNEL_CONFIG="SkyHigh_G920P_defconfig";
+fi;
+
+# build script
+export USER=`whoami`;
+export TMPFILE=`mktemp -t`;
+
+
+# system compiler
+export CROSS_COMPILE=/home/buildserver/android/toolchains/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-gnu-
+
+
+# CPU Core
+export NUMBEROFCPUS=`grep 'processor' /proc/cpuinfo | wc -l`;